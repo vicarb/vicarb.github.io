2f62cd8dd4aa1d831a33f4ad959b52ffaabb482c
vicarb <108682674+vicarb@users.noreply.github.com>
2023-03-31 12:14:47 -0300

    Update Experience.tsx

--- components/Experience/Experience.tsx
+++ components/Experience/Experience.tsx
@@ -12,17 +12,39 @@ const roboto = Roboto({
   weight: ['300'],
 });
 
-
+const experienceContent = [
+  {
+    title: "Desarrollador Full-Stack",
+    company: "Acme Inc.",
+    date: "Enero 2022 - Presente",
+    description: "Desarrollo y mantenimiento de aplicaciones web y móviles, incluyendo un sitio de comercio electrónico y una aplicación móvil para iOS y Android."
+  },
+  {
+    title: "Desarrollador Front-End",
+    company: "XYZ Corp.",
+    date: "Septiembre 2020 - Diciembre 2021",
+    description: "Desarrollo de extensiones de Chrome y aplicaciones web para clientes corporativos."
+  }
+];
 
 export const Experience = () => {
   return (
-    <div>
-    <h1 className={`${roboto.className} text-2xl text-white`}>Experiencia</h1>
-    <h1 className={`${inter.className} text-white text-xl`}>Ecommerce Deploy & Analytics</h1>
-    <h1 className={`${inter.className} text-white text-xl`}>IOS/Android Apps
-    </h1>
-    <h1 className={`${inter.className} text-white text-xl`}>Chrome Extensions
-    </h1>
+    <div className="py-12 bg-gray-900 rounded-lg">
+      <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+        <h1 className={`${roboto.className} text-3xl font-bold text-white mb-4`}>Experiencia</h1>
+        <div className="space-y-8">
+          {experienceContent.map((item, index) => (
+            <div key={index} className="p-6 bg-gray-800 rounded-lg">
+              <div className="flex justify-between items-center mb-2">
+                <h2 className={`${inter.className} text-lg font-bold text-white`}>{item.title}</h2>
+                <p className={`${inter.className} text-gray-400 text-sm`}>{item.date}</p>
+              </div>
+              <h3 className={`${inter.className} text-gray-400 text-sm mb-2`}>{item.company}</h3>
+              <p className={`${inter.className} text-white leading-6`}>{item.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
