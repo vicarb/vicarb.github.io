aa32a225e228cb5a4b08bb358267458d026efcaf
vicarb <vcardenasb@protonmail.com>
2023-04-27 12:47:22 -0400

    contact form mobile design fix

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,11 @@
-self.__NEXT_FONT_MANIFEST={pages:{"/":["static/media/e32130b794b789cb-s.p.woff2","static/media/7d8c9b0ca4a64a5a-s.p.woff2"]},app:{},appUsingSizeAdjust:!1,pagesUsingSizeAdjust:!0};
+self.__NEXT_FONT_MANIFEST={
+  "pages": {
+    "/": [
+      "static/media/e32130b794b789cb-s.p.woff2",
+      "static/media/7d8c9b0ca4a64a5a-s.p.woff2"
+    ]
+  },
+  "app": {},
+  "appUsingSizeAdjust": false,
+  "pagesUsingSizeAdjust": true
+}
--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -58,42 +58,43 @@ const Contact = () => {
   }
 
   return (
-    <div className="a p-12 sm:px-6 lg:px-8">
-    <div className="max-w-md md:max-w-lg mx-auto bg-blue-500 rounded-md shadow-md overflow-hidden">
-      <h2 className="text-center text-3xl font-semibold py-6 text-slate-50">Contact Me!</h2>
-      <form onSubmit={handleSubmit} className="px-8 py-6 space-y-6 bg-blue-500">
-        <div>
-          <label htmlFor="name" className="block text-slate-100 font-medium mb-2 text-lg">Name</label>
-          <input type="text" id="name" name="name" className="w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50 py-2 px-4 text-gray-800 text-lg" required value={formData.name} onChange={handleChange} />
-        </div>
-        <div>
-          <label htmlFor="email" className="block text-slate-100 font-medium mb-2 text-lg">Email</label>
-          <input type="email" id="email" name="email" className="w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50 py-2 px-4 text-gray-800 text-lg" required value={formData.email} onChange={handleChange} />
-        </div>
-        <div>
-          <label htmlFor="message" className="block text-slate-100 font-medium mb-2 text-lg">Message</label>
-          <textarea id="message" name="message" rows={5} className="w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50 py-2 px-4 text-gray-800 text-lg" required value={formData.message} onChange={handleChange}></textarea>
-        </div>
-        <div>
-          <button type="submit" className="w-full bg-indigo-500 hover:bg-indigo-600 text-white font-medium py-2 px-4 rounded-md text-lg">Send Message</button>
-        </div>
-        {isLoading && (
-          <div className="flex justify-center items-center">
-            <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-              <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-            </svg>
-            <p className="text-white font-medium">Submitting...</p>
-          </div>
-        )}
-      </form>
-      {isSuccess && (
-        <div className="px-8 py-6 bg-green-500 text-white text-center">
-          <p className="font-medium text-lg">Message sent!</p>
-          <button  onClick={() => setIsSuccess(false)} className="mt-4 bg-white text-green-500 font-medium py-2 px-4 rounded-md text-lg">Done</button>
+    <div className="a p-4 sm:p-12 sm:px-6 lg:px-8">
+  <div className="max-w-md md:max-w-lg mx-auto bg-blue-500 rounded-md shadow-md overflow-hidden">
+    <h2 className="text-center text-3xl font-semibold py-6 text-slate-50">Contact Me!</h2>
+    <form onSubmit={handleSubmit} className="px-4 sm:px-8 py-6 space-y-4 sm:space-y-6 bg-blue-500">
+      <div>
+        <label htmlFor="name" className="block text-slate-100 font-medium mb-2 text-lg">Name</label>
+        <input type="text" id="name" name="name" className="w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50 py-2 px-3 sm:px-4 text-gray-800 text-lg" required value={formData.name} onChange={handleChange} />
+      </div>
+      <div>
+        <label htmlFor="email" className="block text-slate-100 font-medium mb-2 text-lg">Email</label>
+        <input type="email" id="email" name="email" className="w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50 py-2 px-3 sm:px-4 text-gray-800 text-lg" required value={formData.email} onChange={handleChange} />
+      </div>
+      <div>
+        <label htmlFor="message" className="block text-slate-100 font-medium mb-2 text-lg">Message</label>
+        <textarea id="message" name="message" rows={5} className="w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50 py-2 px-3 sm:px-4 text-gray-800 text-lg" required value={formData.message} onChange={handleChange}></textarea>
+      </div>
+      <div>
+        <button type="submit" className="w-full bg-indigo-500 hover:bg-indigo-600 text-white font-medium py-2 px-3 sm:px-4 rounded-md text-lg">Send Message</button>
+      </div>
+      {isLoading && (
+        <div className="flex justify-center items-center">
+          <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+          </svg>
+          <p className="text-white font-medium">Submitting...</p>
         </div>
       )}
-    </div>
+    </form>
+    {isSuccess && (
+      <div className="px-4 sm:px-8 py-4 sm:py-6 bg-green-500 text-white text-center">
+        <p className="font-medium text-lg">Message sent!</p>
+        <button onClick={() => setIsSuccess(false)} className="mt-4 bg-white text-green-500 font-medium py-2 px-3 sm:px-4 rounded-md text-lg">Done</button>
+      </div>
+    )}
   </div>
+</div>
+
   
 
 
